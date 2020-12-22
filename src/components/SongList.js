4fd146e7b2a24b037279be695ba88b87e664f584
sai <pallesp@amazon.com>
2020-12-21 22:23:31 -0800

    Add SongList and Connect components

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class SongList extends  React.Component {
+
+    render() {
+        return (
+            <div>
+                SongList
+            </div>
+        );
+    }
+}
+
+export default connect()(SongList);
