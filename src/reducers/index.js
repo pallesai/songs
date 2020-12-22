06fe8b2a42e7638f62dc7840b9290789a60eb708
sai <pallesp@amazon.com>
2020-12-21 22:10:50 -0800

    Add Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+const songsReducer = () => {
+    return [
+        {
+            title: 'No Scrubs',
+            duration: '2.30'
+        },
+        {
+            title: 'BackStreet boys',
+            duration: '4.20'
+        },
+        {
+            title: 'Maaya ',
+            duration: '2.00'
+        }
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+}
